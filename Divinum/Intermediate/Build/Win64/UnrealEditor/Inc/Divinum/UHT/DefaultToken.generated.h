185cf757a78fd7ccd98a6c5964c9a92825565f6c
blunte3 <csblunt030@gmail.com>
2023-10-24 15:00:10 -0400

    added default c++ class + git LFS
    Integrated visual studio c++ class with unreal engine + solution files. Added git LFS and tracking files to use with gitignore

---
+++ Divinum/Intermediate/Build/Win64/UnrealEditor/Inc/Divinum/UHT/DefaultToken.generated.h
@@ -0,0 +1,64 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "DefaultToken.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef DIVINUM_DefaultToken_generated_h
+#error "DefaultToken.generated.h already included, missing '#pragma once' in DefaultToken.h"
+#endif
+#define DIVINUM_DefaultToken_generated_h
+
+#define FID_DIVINUM_VIDEOGAME_divinum_Divinum_Source_Divinum_DefaultToken_h_12_SPARSE_DATA
+#define FID_DIVINUM_VIDEOGAME_divinum_Divinum_Source_Divinum_DefaultToken_h_12_SPARSE_DATA_PROPERTY_ACCESSORS
+#define FID_DIVINUM_VIDEOGAME_divinum_Divinum_Source_Divinum_DefaultToken_h_12_EDITOR_ONLY_SPARSE_DATA_PROPERTY_ACCESSORS
+#define FID_DIVINUM_VIDEOGAME_divinum_Divinum_Source_Divinum_DefaultToken_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define FID_DIVINUM_VIDEOGAME_divinum_Divinum_Source_Divinum_DefaultToken_h_12_ACCESSORS
+#define FID_DIVINUM_VIDEOGAME_divinum_Divinum_Source_Divinum_DefaultToken_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesADefaultToken(); \
+	friend struct Z_Construct_UClass_ADefaultToken_Statics; \
+public: \
+	DECLARE_CLASS(ADefaultToken, APawn, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/Divinum"), NO_API) \
+	DECLARE_SERIALIZER(ADefaultToken)
+
+
+#define FID_DIVINUM_VIDEOGAME_divinum_Divinum_Source_Divinum_DefaultToken_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ADefaultToken(ADefaultToken&&); \
+	NO_API ADefaultToken(const ADefaultToken&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ADefaultToken); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ADefaultToken); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(ADefaultToken) \
+	NO_API virtual ~ADefaultToken();
+
+
+#define FID_DIVINUM_VIDEOGAME_divinum_Divinum_Source_Divinum_DefaultToken_h_9_PROLOG
+#define FID_DIVINUM_VIDEOGAME_divinum_Divinum_Source_Divinum_DefaultToken_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_DIVINUM_VIDEOGAME_divinum_Divinum_Source_Divinum_DefaultToken_h_12_SPARSE_DATA \
+	FID_DIVINUM_VIDEOGAME_divinum_Divinum_Source_Divinum_DefaultToken_h_12_SPARSE_DATA_PROPERTY_ACCESSORS \
+	FID_DIVINUM_VIDEOGAME_divinum_Divinum_Source_Divinum_DefaultToken_h_12_EDITOR_ONLY_SPARSE_DATA_PROPERTY_ACCESSORS \
+	FID_DIVINUM_VIDEOGAME_divinum_Divinum_Source_Divinum_DefaultToken_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_DIVINUM_VIDEOGAME_divinum_Divinum_Source_Divinum_DefaultToken_h_12_ACCESSORS \
+	FID_DIVINUM_VIDEOGAME_divinum_Divinum_Source_Divinum_DefaultToken_h_12_INCLASS_NO_PURE_DECLS \
+	FID_DIVINUM_VIDEOGAME_divinum_Divinum_Source_Divinum_DefaultToken_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> DIVINUM_API UClass* StaticClass<class ADefaultToken>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_DIVINUM_VIDEOGAME_divinum_Divinum_Source_Divinum_DefaultToken_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ Divinum/Intermediate/Build/Win64/UnrealEditor/Inc/Divinum/UHT/Divinum.init.gen.cpp
@@ -0,0 +1,29 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeDivinum_init() {}
+	static FPackageRegistrationInfo Z_Registration_Info_UPackage__Script_Divinum;
+	FORCENOINLINE UPackage* Z_Construct_UPackage__Script_Divinum()
+	{
+		if (!Z_Registration_Info_UPackage__Script_Divinum.OuterSingleton)
+		{
+			static const UECodeGen_Private::FPackageParams PackageParams = {
+				"/Script/Divinum",
+				nullptr,
+				0,
+				PKG_CompiledIn | 0x00000000,
+				0xCB52F20A,
+				0xD773F3E9,
+				METADATA_PARAMS(0, nullptr)
+			};
+			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_Divinum.OuterSingleton, PackageParams);
+		}
+		return Z_Registration_Info_UPackage__Script_Divinum.OuterSingleton;
+	}
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_Divinum(Z_Construct_UPackage__Script_Divinum, TEXT("/Script/Divinum"), Z_Registration_Info_UPackage__Script_Divinum, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xCB52F20A, 0xD773F3E9));
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
